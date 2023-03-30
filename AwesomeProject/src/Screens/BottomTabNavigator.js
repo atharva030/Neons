4b91d40cdf7beaef790b43590c743ace5589b781
atharva030 <atharvas_jagdale@auton.in>
2023-03-30 23:49:23 +0530

    UI Ready

--- AwesomeProject/src/Screens/BottomTabNavigator.js
+++ AwesomeProject/src/Screens/BottomTabNavigator.js
@@ -1,4 +1,4 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -13,54 +13,64 @@ import ProfileScreen from './ProfileScreen';
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
-
 function BottomTabNavigator() {
-    return (
-      <Tab.Navigator 
-        screenOptions={({route}) => ({ 
-          headerShown: false,
-          tabBarActiveTintColor: Colors.primary,
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        headerShown: false,
+        tabBarActiveTintColor: Colors.primary,
 
-          tabBarIcon: ({color, size, focused}) => {
-            let iconName;
+        tabBarIcon: ({color, size, focused}) => {
+          let iconName;
 
-            if(route.name == "Home"){
-              iconName = focused ? "home" : 'home-outline';
-            }else if(route.name == "AddTask"){
-              iconName = focused ? 'plus-circle' : 'plus-circle-outline';
-            }else if(route.name == "Add TeamMember"){
-              iconName = focused ? 'account-plus' : 'account-plus-outline';
-            }else if(route.name == "Profile"){
-              iconName = focused ? "account-circle" : 'account-circle-outline';
-            }
-            return <Icon name={iconName} size={20} color={color} style={styles.iconStyle} />
-          },
-          })}>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="AddTask" component={TaskStack} />
-        <Tab.Screen name="Add TeamMember" component={AddTeamMember} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
-    );
-  }
-
-function TaskStack(){
-    return(
-        <Stack.Navigator>
-        <Stack.Screen name="AddTask" component={AddTask} options={{headerShown: false}} />
-        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
-             
-    </Stack.Navigator>
-    );
+          if (route.name == 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name == 'AddTask') {
+            iconName = focused ? 'plus-circle' : 'plus-circle-outline';
+          } else if (route.name == 'Add TeamMember') {
+            iconName = focused ? 'account-plus' : 'account-plus-outline';
+          } else if (route.name == 'Profile') {
+            iconName = focused ? 'account-circle' : 'account-circle-outline';
+          }
+          return (
+            <Icon
+              name={iconName}
+              size={20}
+              color={color}
+              style={styles.iconStyle}
+            />
+          );
+        },
+      })}>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="AddTask" component={TaskStack} />
+      <Tab.Screen name="Add TeamMember" component={AddTeamMember} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
 }
 
+function TaskStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="AddTask"
+        component={AddTask}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+}
 
 const styles = StyleSheet.create({
-  
   iconStyle: {
     marginBottom: -5, // adjust this value to align the icon with the text label
   },
 });
-
 
 export default BottomTabNavigator;
