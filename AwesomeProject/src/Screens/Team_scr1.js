9216ddfe1b46f433ed508b41da1301838172798a
hindavilande05 <landehindavi05@gmail.com>
2023-04-21 21:53:03 +0530

    floating add button added in team_scr1 screen

--- AwesomeProject/src/Screens/Team_scr1.js
+++ AwesomeProject/src/Screens/Team_scr1.js
@@ -1,64 +1,85 @@
-import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import { useState } from 'react'
-import { FAB, Portal, Provider } from 'react-native-paper'
-import styles from '../Styles/Home';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import React from 'react';
+import {useState} from 'react';
+import {FAB, Portal, Provider, TextInput} from 'react-native-paper';
+import styles from '../Styles/Teams1';
+
 import Icon from 'react-native-vector-icons/Ionicons';
+import TeamNames from '../Components/Teams/TeamNames';
 
-const Team_scr1 = ({ navigation }) => {
-    const [state, setState] = useState({ open: false });
-    const onStateChange = ({ open }) => setState({ open });
-    const { open } = state;
-    return (
-        <ScrollView>
-            <View style={styles.Addfullscreen}>
-                <View style={styles.Addsubscreen}>
-                    <TouchableOpacity
-                        style={{ flexDirection: 'row', marginTop: 20 }}
-                        onPress={() => navigation.goBack()}>
-                        <Icon name="chevron-back" size={30} color="white" />
-                        <Text style={styles.AddtitleText}>Add Task</Text>
-                    </TouchableOpacity>
-                </View>
+const Team_scr1 = ({navigation}) => {
+  const [state, setState] = useState({open: false});
+  const onStateChange = ({open}) => setState({open});
+  const {open} = state;
+  return (
+    <ScrollView>
+      <View style={styles.Addfullscreen}>
+        <View style={styles.Addsubscreen}>
+          <TouchableOpacity
+            style={{flexDirection: 'row', marginTop: 20}}
+            onPress={() => navigation.goBack()}>
+            <Icon name="chevron-back" size={30} color="white" />
+            <Text style={styles.AddtitleText}>Teams</Text>
+          </TouchableOpacity>
+        </View>
 
-                <View style={styles.addSecondScreen}>
-                    <Provider>
-                        <Portal>
-                            <FAB.Group
-                                open={open}
-                                visible
-                                icon={open ? 'chevron-down' : 'plus'}
-                                actions={[
-                                    {
-                                        icon: 'account-plus',
-                                        label: 'New Member',
-                                        onPress: () => console.log('Pressed star'),
-                                    },
-                                    {
-                                        icon: 'account-check',
-                                        label: 'Existing Member',
-                                        onPress: () => console.log('Pressed email'),
-                                    }
+        <View style={styles.addSecondScreen}>
+          {/* Floting add icon   */}
 
-                                ]}
-                                onStateChange={onStateChange}
-                                onPress={() => {
-                                    if (open) {
-                                        // do something if the speed dial is open
-                                    }
-                                }}
-                            />
-                        </Portal>
-                    </Provider>
-                </View>
-            </View>
-        </ScrollView>
+          <Provider>
+
+
+          <Portal>
+              <Modal
+                visible={visible}
+                onDismiss={hideModal}
+                contentContainerStyle={styles.containerStyle}>
+                <Icon
+                  name="checkmark-circle-outline"
+                  size={90}
+                  color="#008000"
+                />
+                <Text style={styles.modalTital}>Congrats !</Text>
+                <Text style={styles.modalSubtital}>
+                  You successfully created a task in your account.
+                </Text>
+              </Modal>
+            </Portal>
 
 
 
+            <Portal>
+              <FAB.Group
+                open={open}
+                visible
+                icon={open ? 'chevron-down' : 'plus'}
+                actions={[
+                  {
+                    icon: 'account-plus',
+                    label: 'New Team',
+                    onPress: () => console.log('Pressed star'),
+                  },
+                ]}
+                onStateChange={onStateChange}
+                onPress={() => {
+                  if (open) {
+                    // do something if the speed dial is open
+                  }
+                }}
+              />
+            </Portal>
+          </Provider>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
 
-    );
-}
-
-
-export default Team_scr1
+export default Team_scr1;
